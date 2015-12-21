f674b92fae42bf0f4c8b38bfa9eb8ac6cc3c10f5
StacyJCarlos <sjcarlos212@gmail.com>
2015-12-21 10:51:25 -0500

    Duplicate Test Name commit
    This is the project commit for the duplicate test name bug encountered.

---
+++ DuplicateTestName/Scripts/app/components/applyAccountDistributionSetForm.controller.spec.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    describe('applyProjectDistributionSetForm.controller:', function () {
+        describe('apply', function () {
+            it('setId is less than 0', function () {
+                expect(true).toEqual(true);
+            });
+            it('setId is 0', function () {
+                expect(true).toEqual(true);
+            });
+            it('setId is 2', function () {
+                expect(true).toEqual(true);
+            });
+        });
+    });
+})();
---
+++ DuplicateTestName/Scripts/app/components/applyProjectDistributionSetForm.controller.spec.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    describe('applyAccountDistributionSetForm.controller:', function () {
+        describe('apply', function () {
+            it('setId is less than 0', function () {
+                expect(true).toEqual(true);
+            });
+            it('setId is 0', function () {
+                expect(true).toEqual(true);
+            });
+            it('setId is 2', function () {
+                expect(true).toEqual(true);
+            });
+        });
+    });
+})();
